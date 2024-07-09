4cd1a8576920d2c23e6bd222e7ecf00998f66960
Oscar Poco <123397536+Oscarpoco@users.noreply.github.com>
2024-07-09 10:03:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <nav>
+      <div className='circle-header'>
+        <div className='circle'>
+
+        </div>
+      </div>
+      <div className='shapes-header'>
+        <div className='rectangle'>
+
+        </div>
+
+        <div className='square'>
+
+        </div>
+        <div className='rectangle'>
+
+        </div>
+        <div className='circle'>
+
+        </div>
+      </div>
+     </nav>
+     <main>Main</main>
+     <div id='content1'>
+
+     </div>
+     <div id='content2'>
+
+     </div>
+     <div id='content3'>
+
+     </div>
+     <footer>
+
+     </footer>
+    </div>
+    
+    
+  );
+}
+
+export default App;
